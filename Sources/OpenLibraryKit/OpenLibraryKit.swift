9d0cda98c6ee1a629904a712a995ac9efffb18a3
Todd Martin <kj4ntv@gmail.com>
2025-06-08 19:16:03 -0700

    fix: mark class as public

--- Sources/OpenLibraryKit/OpenLibraryKit.swift
+++ Sources/OpenLibraryKit/OpenLibraryKit.swift
@@ -16,7 +16,7 @@ enum Size: String {
     case large = "L"
 }
 
-final class OpenLibraryClient: @unchecked Sendable {
+public final class OpenLibraryClient: @unchecked Sendable {
     static let shared = OpenLibraryClient()
 
     private var baseUrl = "https://openlibrary.org/"
@@ -34,7 +34,7 @@ final class OpenLibraryClient: @unchecked Sendable {
     }
 
     private init() {}
-    
+
     func searchByTitle(_ title: String) async throws -> SearchResponse {
         let requestUrl = baseUrl + search + "?title=\(title.urlSafeString)"
         do {
@@ -43,7 +43,7 @@ final class OpenLibraryClient: @unchecked Sendable {
             throw error
         }
     }
-    
+
     func searchByAuthor(_ author: String) async throws -> SearchResponse {
         let requestUrl = baseUrl + search + "?author=\(author.urlSafeString)&sort=new"
         do {
@@ -52,46 +52,46 @@ final class OpenLibraryClient: @unchecked Sendable {
             throw error
         }
     }
-    
+
     private func search(url: String) async throws -> SearchResponse {
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "GET"
         request.setValue(_userAgent, forHTTPHeaderField: "User-Agent")
-        
+
         let (data, response) = try await URLSession.shared.data(for: request)
-        
+
         guard let httpResponse = response as? HTTPURLResponse else {
             throw OpenLibraryError.invalidResponse
         }
-        
+
         if httpResponse.statusCode != 200 {
             throw OpenLibraryError.httpError(httpResponse.statusCode)
         }
-        
+
         let decoder = JSONDecoder()
         return try decoder.decode(SearchResponse.self, from: data)
     }
-    
+
     func isbnSearch(isbn: String) async throws -> BookRecordResponse {
         let url = baseUrl + isbnPath + isbn.isbnQueryable
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "GET"
         request.setValue(_userAgent, forHTTPHeaderField: "User-Agent")
-        
+
         let (data, response) = try await URLSession.shared.data(for: request)
-        
+
         guard let httpResponse = response as? HTTPURLResponse else {
             throw OpenLibraryError.invalidResponse
         }
-        
+
         if httpResponse.statusCode != 200 {
             throw OpenLibraryError.httpError(httpResponse.statusCode)
         }
-        
+
         let decoder = JSONDecoder()
         return try decoder.decode(BookRecordResponse.self, from: data)
     }
-    
+
     func getCoverUrl(isbn: String, size: Size = Size.small) -> String {
         return "https://covers.openlibrary.org/b/isbn/\(isbn)-\(size.rawValue).jpg"
     }
